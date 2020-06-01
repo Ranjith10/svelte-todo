db717e4c2d34fa78865bb25dc4b50465df91012a
Ranjith kumar <ranjit4072@gmail.com>
2020-06-01 19:03:16 +0530

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Todo from "./Todo.svelte";
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+
+<main>
+	<h1>Todo Manager</h1>
+	<Todo />
+</main>
---
+++ Todo.svelte
@@ -0,0 +1,21 @@
+<script>
+  let todo = "";
+  let todoList = [];
+  function handleTodoSubmit() {
+    console.log(todo);
+    todoList = [...todoList, todo];
+    console.log(todoList);
+    todo = "";
+  }
+</script>
+
+<style>
+</style>
+
+<form on:submit|preventDefault={handleTodoSubmit}>
+  <input 
+    bind:value={todo}
+    type="text"
+    placeholder="What needs to be done?"
+  />
+</form>
