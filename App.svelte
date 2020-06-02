1f8b8297aebd91708edb42701b73759a598b3bdd
Ranjith kumar <ranjit4072@gmail.com>
2020-06-02 16:03:26 +0530

    Styled the todo input

--- App.svelte
+++ App.svelte
@@ -7,9 +7,15 @@
     font-family: sans-serif;
     text-align: center;
   }
+  .todo-header {
+    color: rgba(175, 47, 47, 0.45);
+    font-weight: 600;
+    font-size: 34px;
+    text-align: center;
+  }
 </style>
 
 <main>
-	<h1>Todo Manager</h1>
+	<h1 class="todo-header">Todo Manager</h1>
 	<Todo />
 </main>
--- Todo.svelte
+++ Todo.svelte
@@ -2,20 +2,39 @@
   let todo = "";
   let todoList = [];
   function handleTodoSubmit() {
-    console.log(todo);
     todoList = [...todoList, todo];
-    console.log(todoList);
     todo = "";
   }
 </script>
 
 <style>
+  .todo-input {
+    margin: 30px 0px 0;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1);
+    background: #ffffff;
+    width: calc(100% - 60px);
+    min-height: 50px;
+    border: none;
+    padding: 10px 10px 10px 50px;
+    font-size: 22px;
+  }
+  .todo-input-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .todo-input-container > form {
+    width: 550px;
+  }
 </style>
 
-<form on:submit|preventDefault={handleTodoSubmit}>
-  <input 
-    bind:value={todo}
-    type="text"
-    placeholder="What needs to be done?"
-  />
-</form>
+<div class="todo-input-container">
+  <form on:submit|preventDefault={handleTodoSubmit}>
+    <input 
+      class="todo-input"
+      bind:value={todo}
+      type="text"
+      placeholder="What needs to be done?"
+    />
+  </form>
+</div>
